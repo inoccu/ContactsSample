b392cb3a6f8467e15ba0d1f26ea2779250522eb5
Kenichi Inoue <inoccu@gmail.com>
2013-12-22 16:09:43 +0900

    first commit

---
+++ www/js/index.js
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        alert("init");
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        var options = new ContactFindOptions();
+        options.filter = "Taylor";
+        var fields = ["displayName", "name"];
+        navigator.contacts.find(fields, app.onSuccess, app.onError, options);
+    },
+    onSuccess: function(contacts) {
+        for (var i=0; i < contacts.length; i++) {
+            var contact = contacts[i];
+            console.log(JSON.stringify(contact));
+        }
+    },
+    onError: function(contactError) {
+        alert("Error!");
+    }
+};
